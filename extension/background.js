dda8cac442859c27492cf815e2509d259962a589
Frederick Bell <shortstop4313@gmail.com>
2023-08-08 11:16:01 -0400

    Placeholder for failed code

--- extension/background.js
+++ extension/background.js
@@ -1,41 +1,83 @@
+const CSS = "body { border: 20px solid red; }";
+const TITLE_APPLY = "Apply CSS";
+const TITLE_REMOVE = "Remove CSS";
+const APPLICABLE_PROTOCOLS = ["http:", "https:"];
+
 /*
-Called when the item has been created, or when creation failed due to an error.
-We'll just log success/failure here.
+Toggle CSS: based on the current title, insert or remove the CSS.
+Update the page action's title and icon to reflect its state.
 */
-function onCreated() {
-    if (browser.runtime.lastError) {
-        console.debug(`Error: ${browser.runtime.lastError}`);
-    } else {
-        console.debug("Item created successfully");
+function toggleCSS(tab) {
+    function gotTitle(title) {
+        if (title === TITLE_APPLY) {
+            browser.pageAction.setIcon({ tabId: tab.id, path: "icons/on.svg" });
+            browser.pageAction.setTitle({ tabId: tab.id, title: TITLE_REMOVE });
+            getCanonicalUrl();
+        } else {
+            browser.pageAction.setIcon({ tabId: tab.id, path: "icons/off.svg" });
+            browser.pageAction.setTitle({ tabId: tab.id, title: TITLE_APPLY });
+            getCanonicalUrl();
+        }
     }
-    console.clear();
+
+    let gettingTitle = browser.pageAction.getTitle({ tabId: tab.id });
+    gettingTitle.then(gotTitle);
 }
 
 /*
-Called when there was an error.
-We'll just log the error here.
+Returns true only if the URL's protocol is in APPLICABLE_PROTOCOLS.
+Argument url must be a valid URL string.
 */
-function onError(error) {
-    console.debug(`Error: ${error}`);
+function protocolIsApplicable(url) {
+    const protocol = (new URL(url)).protocol;
+    alert(protocol);
+    console.log(protocol);
+    return APPLICABLE_PROTOCOLS.includes(protocol);
 }
+
+/*
+Initialize the page action: set icon and title, then show.
+Only operates on tabs whose URL's protocol is applicable.
+*/
+function initializePageAction(tab) {
+    if (protocolIsApplicable(tab.url)) {
+        browser.pageAction.setIcon({ tabId: tab.id, path: "icons/off.svg" });
+        browser.pageAction.setTitle({ tabId: tab.id, title: TITLE_APPLY });
+        browser.pageAction.show(tab.id);
+    }
+}
+
+/*
+When first loaded, initialize the page action for all tabs.
+*/
+let gettingAllTabs = browser.tabs.query({});
+gettingAllTabs.then((tabs) => {
+    for (let tab of tabs) {
+        initializePageAction(tab);
+    }
+});
+
+/*
+Each time a tab is updated, reset the page action for that tab.
+*/
+browser.tabs.onUpdated.addListener((id, changeInfo, tab) => {
+    initializePageAction(tab);
+});
+
+/*
+Toggle CSS when the page action is clicked.
+*/
+browser.pageAction.onClicked.addListener(toggleCSS);
 
 function getCanonicalUrl() {
     var canonical = "";
-    alert('taco hell');
-    if (window.location.href.indexOf("lessonID") > -1) {
-        var links = document.getElementsByTagName("link");
-        alert('all good');
-        for (var i = 0; i < links.length; i++) {
-            if (links[i].getAttribute("rel") === "canonical") {
-                canonical = links[i].getAttribute("href")
-            }
+    var links = document.getElementsByTagName("link");
+    console.log(document);
+    for (var i = 0; i < links.length; i++) {
+        if (links[i].getAttribute("rel") === "canonical") {
+            canonical = links[i].getAttribute("href")
         }
-        alert(canonical);
     }
-    return canonical;
+    canonical = document.querySelector("link[rel='canonical']").getAttribute("href");
+    window.open(canonical);
 }
-document.addEventListener('DOMContentLoaded', function () {
-
-    window.open(getCanonicalUrl());
-    alert('done getting')
-}, false);
